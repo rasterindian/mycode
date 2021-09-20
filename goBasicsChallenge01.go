cc0a208ca9394bd9981921be805e6ffc7a3f0c82
Rakshit Yagnik <yagnik@gmail.com>
2021-09-20 18:02:27 +0000

    learning about go basics

---
+++ goBasicsChallenge01.go
@@ -0,0 +1,14 @@
+/* Alta3 Research | RZFeeser
+   Exported names   */
+   
+package main
+
+import (
+    "fmt"
+)
+
+func main() {
+    fmt.Println("Rakshit Yagnik")   // notice that "Pi" is capitalized
+    fmt.Println("Bedminster, NJ")
+}
+
